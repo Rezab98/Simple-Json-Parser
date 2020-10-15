aacb1f38f83a698ca68a99a88f6b708ff8220bcf
reza <reza.baghaee@carriot.ir>
2020-10-15 20:43:34 +0330

    second commit

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 #include <string>
 
 int nodes[100];
-int nodes[100];
+int depths[100];
+int reducedNodes[100];
+int bfs[100];
+int lpar[100];
+parent[100];
+nchild[100];
+alloc[100];
 int expressionLength = expression.length()
 bool CheckIfParanthesIsBalanced(string expression){
     int sum = 0
@@ -32,9 +38,78 @@ void CountDepth(string expression){
     for(int i = 0; i < expressionLength; i++){
         if(expression[i] == '(')
             depth++;
-        nodes[i] = node;
+        else
+            depth--;
+        depths[i] = depth;
     }
 }
+//todo : check if it is neccessary
+int ReduceToNodes(int nodes[], int depths[], strign expression){
+    int i = 0;
+    int j = 0;
+    while(j < expressionLength)
+        if(expression[j] == '(')
+            reducedNodes[i] = depths[j];
+    return i;
+}
+
+int nodesCount = ReduceToNodes(nodes, depths, expression)
+
+
+void SorthByDepth(int reducedNodes[]){
+    pair<int, int> depthAndIndex[100];
+    for(int i = 0; i < nodesCount; i++){
+        p = makepair(depths[i], reducedNodes[i])
+        depthAndIndex[i] = p;
+    }
+    sort(depthAndIndex, depthAndIndex + nodesCount)
+    for(int i = 0; i < nodesCount; i++){
+       for(int j = 0; j < nodesCount; j++)
+           if(depthAndIndex[j].second = i)
+               bfs[i] = j;
+           
+
+}
+void FindParentOfLeftMostCHild(int bfs[],int depths[]){
+    lpar[0] = -1;
+    for(int i = 1; i < nodesCount ; i++){
+        lpar[i] = -1;
+        if(depths[i] - depths[i - 1] == 1)
+            lpar[bfs[i]] = bfs[i - 1];
+    }
+}
+
+void FindParenOfNodes(int lpar[]){
+    parent[0] = -1;
+    for(int i = 1; i < nodesCount; i++){
+        if(lpar[i] == -1)
+            parent[i] = paren[i - 1];
+        else
+            parent[i] = lpar[i];
+    }
+}
+//todo : check if better implemntetion is possible
+void FindNumberOfChildren(int parent[]){
+    for(int i = 0; i < nodesCount; i++)
+        nchild[i] = 0;
+    int numberOfChildren = 1;
+    for(int i = 1; i < nodesCount; i++){
+        if(parent[i] != parent[i - 1] && parent[i - 1] != -1){
+            nchild[parent[i - 1]] = numberOfChildren;
+            numberOfChildren = 1;
+        }
+        else if(paret[i] == parent[i - 1] && parent[i - 1] != -1)
+            numberOfChildren++
+    }
+}
+
+void BuildAllocArray(){
+    alloc[0] = 0;
+    for(int i = 1; i < nodesCount; i++){
+        alloc[i] = alloc[i - 1] + nchild[i - 1] + 1;
+    }
+}
+
 
 
 int main(){
